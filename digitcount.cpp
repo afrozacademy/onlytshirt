5f6affc9b3f749e600967d9a6fc18edde402896d
Shariya jalan <56727282+shriyajalana@users.noreply.github.com>
2022-10-08 13:02:16 +0530

    Update digitcount.cpp

--- digitcount.cpp
+++ digitcount.cpp
@@ -1,17 +1,63 @@
+//stack using linkedlist at the beginning of the list O(1) i.e. constant time complexity
 #include <iostream>
 using namespace std;
-int Count(int n, int count)
+struct Node
 {
-    if (n == 0)
-    {
-        return count;
-    }
-    return Count(n / 10, count + 1);
+    int data;
+    Node *next;
+};
+Node *head;
+
+void PushAtBeginning(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    temp->next = head;
+    head = temp;
 }
+
+void Poping()
+{
+    if (head == NULL)
+    {
+        cout << "Poping not possible\n";
+        return;
+    }
+    Node *temp = head;
+    cout << "Pop item is: " << head->data << endl;
+    head = head->next;
+    free(temp);
+}
+void Print()
+{
+    Node *temp;
+    temp = head;
+    cout << "items is...  ";
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int count = 0;
-    cout << Count(n, count) << endl;
+    head = NULL;
+    Print();
+    Poping();
+    PushAtBeginning(5);
+    Print();
+    Poping();
+    PushAtBeginning(3);
+    Print();
+    PushAtBeginning(7);
+    Print();
+    Poping();
+    PushAtBeginning(4);
+    Print();
+    Poping();
+    Print();
+    return 0;
 }
